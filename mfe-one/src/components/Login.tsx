1a3caa1e809011128e0cd6e52125cbc993efd23b
Sannith <sannithpeddolla143@gmail.com>
2025-01-20 22:47:01 +0530

    Default structure for demo

--- mfe-one/src/components/Login.tsx
+++ mfe-one/src/components/Login.tsx
@@ -1,161 +1,173 @@
-import React, { FunctionComponent } from "react";
-import { Arrows } from "../Arrows/Arrows";
-import { Click } from "../Click/Click";
-import { Vector } from "../Vector/Vector";
-import "../../../styleguide1.css";
+// import React, { FunctionComponent } from "react";
+// import { Arrows } from "../Arrows/Arrows";
+// import { Click } from "../Click/Click";
+// import { Vector } from "../Vector/Vector";
+// import "../../../styleguide1.css";
 
-import "./style.css";
-// import universalStudiosHollywood1 from "./universal-studios-hollywood-1.png";
+// import "./style.css";
+// // import universalStudiosHollywood1 from "./universal-studios-hollywood-1.png";
 
-const Login : FunctionComponent = () =>  {
+// const Login : FunctionComponent = () =>  {
   
-  return (
-    <div className="login">
-      <div className="section">
-        <div className="container">
-          <div className="content">
-            {/* <Vector className="vector-instance" /> */}
-            <img
-      className="vector-instance"
-      alt="Universal Studios"
-      style={{
-        content: `url(${getComputedStyle(document.documentElement).getPropertyValue('--global-logo-value').trim()})`
-      }}
-    />
-            <div className="text-and-supporting">
-              <div className="text">Welcome to RAG</div>
+//   return (
+//     <div className="login">
+//       <div className="section">
+//         <div className="container">
+//           <div className="content">
+//             {/* <Vector className="vector-instance" /> */}
+//             <img
+//       className="vector-instance"
+//       alt="Universal Studios"
+//       style={{
+//         content: `url(${getComputedStyle(document.documentElement).getPropertyValue('--global-logo-value').trim()})`
+//       }}
+//     />
+//             <div className="text-and-supporting">
+//               <div className="text">Welcome to RAG</div>
 
-              <div className="supporting-text">Please enter your details.</div>
-            </div>
+//               <div className="supporting-text">Please enter your details.</div>
+//             </div>
 
-            <div className="frame">
-              <div className="div">
-                <div className="form">
-                  <div className="div-2">
-                    <div className="div-2">
-                      <input
-                        className="label-1"
-                        // htmlFor="input-1"
-                        placeholder="Email"
-                        type="email"
-                      />
+//             <div className="frame">
+//               <div className="div">
+//                 <div className="form">
+//                   <div className="div-2">
+//                     <div className="div-2">
+//                       <input
+//                         className="label-1"
+//                         // htmlFor="input-1"
+//                         placeholder="Email"
+//                         type="email"
+//                       />
 
-                      <div className="input">
-                        <input
-                          className="text-wrapper"
-                          id="input-1"
-                          placeholder="Enter your email"
-                          type="email"
-                        />
-                      </div>
-                    </div>
-                  </div>
+//                       <div className="input">
+//                         <input
+//                           className="text-wrapper"
+//                           id="input-1"
+//                           placeholder="Enter your email"
+//                           type="email"
+//                         />
+//                       </div>
+//                     </div>
+//                   </div>
 
-                  <div className="div-2">
-                    <div className="div-2">
-                      <label className="label-2" htmlFor="input-3">
-                        Password
-                      </label>
+//                   <div className="div-2">
+//                     <div className="div-2">
+//                       <label className="label-2" htmlFor="input-3">
+//                         Password
+//                       </label>
 
-                      <div className="input">
-                        <input
-                          className="text-wrapper"
-                          id="input-3"
-                          placeholder="••••••••"
-                          type="text"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                </div>
+//                       <div className="input">
+//                         <input
+//                           className="text-wrapper"
+//                           id="input-3"
+//                           placeholder="••••••••"
+//                           type="text"
+//                         />
+//                       </div>
+//                     </div>
+//                   </div>
+//                 </div>
 
-                <div className="row">
-                  <div className="checkbox">
-                    <div className="checkbox-base-wrapper">
-                      <div className="checkbox-base" />
-                    </div>
+//                 <div className="row">
+//                   <div className="checkbox">
+//                     <div className="checkbox-base-wrapper">
+//                       <div className="checkbox-base" />
+//                     </div>
 
-                    <div className="text-and-supporting-2">
-                      <div className="text-2">Remember for 30 days</div>
-                    </div>
-                  </div>
+//                     <div className="text-and-supporting-2">
+//                       <div className="text-2">Remember for 30 days</div>
+//                     </div>
+//                   </div>
 
-                  <button className="button">
-                    <div className="text-3">Forgot password</div>
-                  </button>
-                </div>
+//                   <button className="button">
+//                     <div className="text-3">Forgot password</div>
+//                   </button>
+//                 </div>
 
-                <div className="actions">
-                  <button className="button-2">
-                    <div className="text-4">Sign in</div>
-                  </button>
-                </div>
-              </div>
-            </div>
+//                 <div className="actions">
+//                   <button className="button-2">
+//                     <div className="text-4">Sign in</div>
+//                   </button>
+//                 </div>
+//               </div>
+//             </div>
 
-            <div className="row-2">
-              <div className="text-5">Don’t have an account?</div>
+//             <div className="row-2">
+//               <div className="text-5">Don’t have an account?</div>
 
-              <button className="button-3">
-                <div className="text-3">Sign up</div>
-              </button>
-            </div>
-{/* 
-            <div className="group">
-              <div className="overlap-group-wrapper">
-                <div className="overlap-group">
-                  <Click className="click-instance" />
-                  <div className="text-wrapper-2">CLICK</div>
-                </div>
-              </div>
-            </div> */}
-          </div>
-        </div>
-      </div>
+//               <button className="button-3">
+//                 <div className="text-3">Sign up</div>
+//               </button>
+//             </div>
+// {/* 
+//             <div className="group">
+//               <div className="overlap-group-wrapper">
+//                 <div className="overlap-group">
+//                   <Click className="click-instance" />
+//                   <div className="text-wrapper-2">CLICK</div>
+//                 </div>
+//               </div>
+//             </div> */}
+//           </div>
+//         </div>
+//       </div>
 
-      <div className="frame-khu">
-        <div className="text-wrapper-3">Your everyday AI companion</div>
+//       <div className="frame-khu">
+//         <div className="text-wrapper-3">Your everyday AI companion</div>
 
-        <div className="frame-2">
-          <div className="frame-3">
-          <img
-      className="universal-studios"
-      alt="Universal Studios"
-      style={{
-        content: `url(${getComputedStyle(document.documentElement).getPropertyValue('--global-image1-value').trim()})`
-      }}
-    />
+//         <div className="frame-2">
+//           <div className="frame-3">
+//           <img
+//       className="universal-studios"
+//       alt="Universal Studios"
+//       style={{
+//         content: `url(${getComputedStyle(document.documentElement).getPropertyValue('--global-image1-value').trim()})`
+//       }}
+//     />
        
             
 
-            <p className="supporting-text-2">
-              Create a highly itinerary with multiple options.
-            </p>
-          </div>
+//             <p className="supporting-text-2">
+//               Create a highly itinerary with multiple options.
+//             </p>
+//           </div>
 
-          <div className="frame-3">
-            {/* <img
-              className="img"
-              alt="Universal studios"
-              src="/bg_image_2.png"
-            /> */}
-               <img
-      className="universal-studios"
-      alt="Universal Studios"
-      style={{
-        content: `url(${getComputedStyle(document.documentElement).getPropertyValue('--global-image2-value').trim()})`
-      }}
-    />
+//           <div className="frame-3">
+//             {/* <img
+//               className="img"
+//               alt="Universal studios"
+//               src="/bg_image_2.png"
+//             /> */}
+//                <img
+//       className="universal-studios"
+//       alt="Universal Studios"
+//       style={{
+//         content: `url(${getComputedStyle(document.documentElement).getPropertyValue('--global-image2-value').trim()})`
+//       }}
+//     />
 
-            <div className="supporting-text-2">
-              Provide current entertainment trends
-            </div>
-          </div>
-        </div>
+//             <div className="supporting-text-2">
+//               Provide current entertainment trends
+//             </div>
+//           </div>
+//         </div>
 
-        <Arrows className="arrows-instance" />
-      </div>
+//         <Arrows className="arrows-instance" />
+//       </div>
+//     </div>
+//   );
+// };
+// export default Login;
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      
     </div>
-  );
-};
-export default Login;
+  )
+}
+
+export default Login
--- mfe-two/src/components/Home1.tsx
+++ mfe-two/src/components/Home1.tsx
@@ -1,168 +1,179 @@
-import React, { FunctionComponent } from "react";
-import { ChatBotSvgrepoCom1 } from "../ChatBotSvgrepoCom1/ChatBotSvgrepoCom1";
-import { Chats } from "../Chats/Chats";
-import { Click } from "../Click/Click";
-import { Frame4410 } from "../Frame4410/Frame4410";
-import { Header } from "../Header/Header";
-import { HistoryIcon1 } from "../HistoryIcon1/HistoryIcon1";
-import { Settings } from "../Settings/Settings";
-import "../../../styleguide1.css";
-// import OBJECTS from "./OBJECTS.png";
-import "./style.css";
+// import React, { FunctionComponent } from "react";
+// import { ChatBotSvgrepoCom1 } from "../ChatBotSvgrepoCom1/ChatBotSvgrepoCom1";
+// import { Chats } from "../Chats/Chats";
+// import { Click } from "../Click/Click";
+// import { Frame4410 } from "../Frame4410/Frame4410";
+// import { Header } from "../Header/Header";
+// import { HistoryIcon1 } from "../HistoryIcon1/HistoryIcon1";
+// import { Settings } from "../Settings/Settings";
+// import "../../../styleguide1.css";
+// // import OBJECTS from "./OBJECTS.png";
+// import "./style.css";
 
-const Home1 : FunctionComponent = () =>  {
+// const Home1 : FunctionComponent = () =>  {
+//   return (
+//     <div className="home">
+//       <div className="overlap-wrapper">
+//         <div className="overlap">
+//           <div className="frame">
+//             <div className="navigation">
+//               <div className="nav">
+//                 <Header className="header-instance" />
+//                 <div className="div">
+//                   <div className="nav-wrapper">
+//                     <div className="nav-2">
+//                       <ChatBotSvgrepoCom1 className="chat-bot-svgrepo-com" />
+//                       <div className="text">New Chat</div>
+//                     </div>
+//                   </div>
+
+//                   <div className="frame-2">
+//                     <div className="nav-3">
+//                       <HistoryIcon1 className="icon-instance-node" />
+//                       <div className="text-wrapper">Chat History</div>
+//                     </div>
+
+//                     <div className="frame-wrapper">
+//                       <div className="frame-3">
+//                         <div className="supporting-text-wrapper">
+//                           <div className="supporting-text-2">Recent Chats</div>
+//                         </div>
+
+//                         <Chats className="chats-instance" property="select" />
+//                       </div>
+//                     </div>
+//                   </div>
+//                 </div>
+//               </div>
+
+//               <footer className="footer">
+//                 <div className="nav-3">
+//                   <Settings className="icon-instance-node" />
+//                   <div className="text-wrapper">Settings</div>
+//                 </div>
+
+//                 <div className="frame-4">
+//                   <div className="avatar">
+//                     <div className="avatar-contrast" />
+//                   </div>
+
+//                   <div className="text-and-supporting">
+//                     <div className="text-2">Olivia Rhye</div>
+
+//                     <div className="supporting-text-3">
+//                       olivia@untitledui.com
+//                     </div>
+//                   </div>
+//                 </div>
+//               </footer>
+//             </div>
+
+//             <div className="div-wrapper">
+//               <div className="frame-5">
+//                 <div className="frame-6">
+//                   {/* <img className="OBJECTS" alt="Objects" src={OBJECTS} /> */}
+
+//                   <div className="text-3">Welcome Olivia</div>
+
+//                   <div className="flexcontainer">
+//                     <p className="span-wrapper">
+//                       <span className="span">
+//                         I&#39;m RAG, your dedicated generative AI companion,
+//                         crafted to enhance the Universal experience from
+//                         within.    
+//                         <br />
+//                       </span>
+//                     </p>
+
+//                     <p className="span-wrapper">
+//                       <span className="span">
+//                         Consider me your behind-the-scenes guide, equipped to
+//                         illuminate the answers and support you need, helping to
+//                         streamline our collective efforts toward unparalleled
+//                         guest experiences.   
+//                         <br />
+//                       </span>
+//                     </p>
+
+//                     <p className="span-wrapper">
+//                       <span className="span">Let the magic begin! </span>
+//                     </p>
+//                   </div>
+//                 </div>
+
+//                 <div className="frame-7">
+//                   <div className="frame-8">
+//                     <div className="frame-8">
+//                       <div className="frame-9">
+//                         <div className="supporting-text-4">
+//                           Creative Writing Assistance
+//                         </div>
+
+//                         <p className="p">
+//                           Help me write a blog post about the future of UX
+//                           Research in the Age of AI.
+//                         </p>
+//                       </div>
+
+//                       <div className="frame-9">
+//                         <div className="supporting-text-4">
+//                           Programming and Coding Help
+//                         </div>
+
+//                         <p className="p">
+//                           I&#39;m struggling with a Python function. How can I
+//                           sort a list of dictionaries by a specific key in each
+//                           dictionary?
+//                         </p>
+//                       </div>
+
+//                       <div className="frame-9">
+//                         <div className="supporting-text-4">
+//                           Company Information
+//                         </div>
+
+//                         <p className="p">
+//                           Who should I contact about submitting my expense
+//                           report?
+//                         </p>
+//                       </div>
+//                     </div>
+//                   </div>
+
+//                   <div className="frame-10">
+//                     <div className="frame-11">
+//                       <div className="supporting-text-5">Start typing...</div>
+
+//                       <Frame4410 className="frame-4410" />
+//                     </div>
+//                   </div>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+
+//           <div className="group">
+//             <div className="overlap-group-wrapper">
+//               <div className="overlap-group">
+//                 <Click className="click-instance" />
+//                 <div className="text-wrapper-2">CLICK</div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+// export default Home1;
+import React from 'react'
+
+function Home1() {
   return (
-    <div className="home">
-      <div className="overlap-wrapper">
-        <div className="overlap">
-          <div className="frame">
-            <div className="navigation">
-              <div className="nav">
-                <Header className="header-instance" />
-                <div className="div">
-                  <div className="nav-wrapper">
-                    <div className="nav-2">
-                      <ChatBotSvgrepoCom1 className="chat-bot-svgrepo-com" />
-                      <div className="text">New Chat</div>
-                    </div>
-                  </div>
-
-                  <div className="frame-2">
-                    <div className="nav-3">
-                      <HistoryIcon1 className="icon-instance-node" />
-                      <div className="text-wrapper">Chat History</div>
-                    </div>
-
-                    <div className="frame-wrapper">
-                      <div className="frame-3">
-                        <div className="supporting-text-wrapper">
-                          <div className="supporting-text-2">Recent Chats</div>
-                        </div>
-
-                        <Chats className="chats-instance" property="select" />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <footer className="footer">
-                <div className="nav-3">
-                  <Settings className="icon-instance-node" />
-                  <div className="text-wrapper">Settings</div>
-                </div>
-
-                <div className="frame-4">
-                  <div className="avatar">
-                    <div className="avatar-contrast" />
-                  </div>
-
-                  <div className="text-and-supporting">
-                    <div className="text-2">Olivia Rhye</div>
-
-                    <div className="supporting-text-3">
-                      olivia@untitledui.com
-                    </div>
-                  </div>
-                </div>
-              </footer>
-            </div>
-
-            <div className="div-wrapper">
-              <div className="frame-5">
-                <div className="frame-6">
-                  {/* <img className="OBJECTS" alt="Objects" src={OBJECTS} /> */}
-
-                  <div className="text-3">Welcome Olivia</div>
-
-                  <div className="flexcontainer">
-                    <p className="span-wrapper">
-                      <span className="span">
-                        I&#39;m RAG, your dedicated generative AI companion,
-                        crafted to enhance the Universal experience from
-                        within.    
-                        <br />
-                      </span>
-                    </p>
-
-                    <p className="span-wrapper">
-                      <span className="span">
-                        Consider me your behind-the-scenes guide, equipped to
-                        illuminate the answers and support you need, helping to
-                        streamline our collective efforts toward unparalleled
-                        guest experiences.   
-                        <br />
-                      </span>
-                    </p>
-
-                    <p className="span-wrapper">
-                      <span className="span">Let the magic begin! </span>
-                    </p>
-                  </div>
-                </div>
-
-                <div className="frame-7">
-                  <div className="frame-8">
-                    <div className="frame-8">
-                      <div className="frame-9">
-                        <div className="supporting-text-4">
-                          Creative Writing Assistance
-                        </div>
-
-                        <p className="p">
-                          Help me write a blog post about the future of UX
-                          Research in the Age of AI.
-                        </p>
-                      </div>
-
-                      <div className="frame-9">
-                        <div className="supporting-text-4">
-                          Programming and Coding Help
-                        </div>
-
-                        <p className="p">
-                          I&#39;m struggling with a Python function. How can I
-                          sort a list of dictionaries by a specific key in each
-                          dictionary?
-                        </p>
-                      </div>
-
-                      <div className="frame-9">
-                        <div className="supporting-text-4">
-                          Company Information
-                        </div>
-
-                        <p className="p">
-                          Who should I contact about submitting my expense
-                          report?
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="frame-10">
-                    <div className="frame-11">
-                      <div className="supporting-text-5">Start typing...</div>
-
-                      <Frame4410 className="frame-4410" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="group">
-            <div className="overlap-group-wrapper">
-              <div className="overlap-group">
-                <Click className="click-instance" />
-                <div className="text-wrapper-2">CLICK</div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div>
+      <h1>Home</h1>
     </div>
-  );
-};
-export default Home1;
+  )
+}
+
+export default Home1
